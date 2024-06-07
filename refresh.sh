c330af2b491040407c8162944992c1eeecc75542
Bart Lute <bart.lute@bloomreach.com>
2024-06-07 15:37:46 +0200

    some (hopefully) convenient scripts

---
+++ refresh.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+docker compose down && \
+ git pull && \
+ docker compose build && \
+ docker compose up -d && \
+ docker compose logs -f
---
+++ to-inadyn.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker compose exec -ti inadyn /bin/sh
